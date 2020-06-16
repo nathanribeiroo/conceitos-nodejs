9289ce499e0922d04cbe7bf5def6ca7677768846
Nathan Ribeiro <nathanribeiroo@me.com>
2020-06-16 00:24:12 -0300

    api rest completed

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('\n======================');
+    console.log('🚀 BACK-END STARTED 🚀');
+    console.log('======================\n');
+});
